4991b49355ed995fbc81f2bec195a84c64c80c0b
Newton <biinewton382@gmail.com>
2017-12-08 12:17:23 +0300

    Install flask-script to run app.

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ import os
 
 
 class Config:
+    """Main configurations class"""
 
 
 class ProdConfig(Config):
+    """Production configuration class that inherits from the main configurations class"""
     pass
 
 
 class DevConfig(Config):
+    """Configuration class for development stage of the app"""
     DEBUG = True
 
 
